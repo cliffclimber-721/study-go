5443c0c24eddf1debca9e0ba151893727520319a
cliffclimber-721 <cliffclimber@g.skku.edu>
2024-01-29 15:45:35 +0900

    main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+// main.go 는 컴파일을 하기 위한 파일이다.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/study-go/sth"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	sth.SayHello()
+}
---
+++ sth/sth.go
@@ -0,0 +1,11 @@
+package sth
+
+import "fmt"
+
+func sayBye() {
+	fmt.Print("Bye!")
+}
+
+func SayHello() {
+	fmt.Println("Hello!")
+}
