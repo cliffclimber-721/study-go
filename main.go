d328870de892d32d5ab1658ef58abc525de438da
cliffclimber-721 <cliffclimber@g.skku.edu>
2024-02-05 17:02:07 +0900

    19602.go

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	s, m, l := fmt.Scan(&s, &m, &l)
+	if res := 1*s + 2*m + 3*l; res > 9 {
+		fmt.Println("happy")
+	}
+	fmt.Println("sad")
 }
