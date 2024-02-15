47f86746115f4f528b3238668fc038d20be15099
cliffclimber-721 <cliffclimber@g.skku.edu>
2024-02-15 11:37:47 +0900

    acc + method

--- banking/account.go
+++ banking/account.go
@@ -1,7 +1,5 @@
 package banking
 
-import "fmt"
-
 type Account struct {
 	Owner   string
 	Balance int
@@ -17,11 +15,19 @@ func NewAccount(owner string) *AnoAccount {
 	return &accs
 }
 
-func forMain() {
-	accountCho := banking.Account{Owner: "chocho", Balance: 1000}
-	accountShin := banking.Account{Owner: "shin"}
-	acc := banking.NewAccount("kangs")
-	fmt.Println(accountCho)
-	fmt.Println(accountShin)
-	fmt.Println(acc)
+// a AnoAccount를 receiver 라고 부른다.
+// Account2에 있는 변수를 사용할건데 이걸 이제 a라고 부르면서 쓸 계획
+// import numpy as np 이면 np 쓴다는 말이랑 같은거다
+func (a AnoAccount) Deposit(amount int) {
+	a.bals += amount
+
 }
+
+//func forMain() {
+//	accountCho := banking.Account{Owner: "chocho", Balance: 1000}
+//	accountShin := banking.Account{Owner: "shin"}
+//	acc := banking.NewAccount("kangs")
+//	fmt.Println(accountCho)
+//	fmt.Println(accountShin)
+//	fmt.Println(acc)
+//}
